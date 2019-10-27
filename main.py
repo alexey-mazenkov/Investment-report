7b62c546472350d781fedb72a72fe25aaa3b2fe5
KKravtsov14 <kostya.kravtsov.2001@mail.ru>
2019-10-27 18:32:20 +0700

    entered my contribution

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Investment report.
 # Developers: A. Mazenkov -,
 # A. Mikhailov - ,
-# K. Kravtsov -
+# K. Kravtsov - 45%
 
 # Разработать программу, позволяющую получать информацию за каждый месяц об инвестициях капитала в течение нескольких
 # лет при ежемесячной капитализации.
 
-term = int(input("Срок размещения капитала (лет):"))  # Input period.
-initial_capital = float(input("Начальный капитал ($):"))  # Initial capital input.
-percent = float(input("Процентная ставка (%/мес.):"))  # Input interest rate.
-infusion = float(input("Инвестиционные вливания ($/мес.):"))  # Input additional investments.
-year_output = float(input("Раз в сколько лет выводить депозит:"))  # Input output step.
+term = int(input("Срок размещения капитала (лет):"))                    # Input period.
+initial_capital = float(input("Начальный капитал ($):"))                # Initial capital input.
+percent = float(input("Процентная ставка (%/мес.):"))                   # Input interest rate.
+infusion = float(input("Инвестиционные вливания ($/мес.):"))            # Input additional investments.
+year_output = float(input("Раз в сколько лет выводить депозит:"))       # Input output step.
 
 all_sum = initial_capital
 
@@ -25,15 +25,15 @@ for i in range(1, term + 1):
         print("| месяц  | инвестиций | за месяц   | капитал    |")
         print("-------------------------------------------------")
 
-    first_sum = all_sum  # Variable for original amount
+    first_sum = all_sum                                                 # Variable for original amount
 
     for j in range(12):
-        all_sum *= (1 + (percent / 100))  # Interest accrual
+        all_sum *= (1 + (percent / 100))                                # Interest accrual
 
-        accruals = all_sum - first_sum  # Monthly accrual
+        accruals = all_sum - first_sum                                  # Monthly accrual
 
-        if i % year_output == 0 or i == term or i == 1:  # If in order to display a table
-            str_1 = str("%.2f" % (first_sum))  # Strings for table output
+        if i % year_output == 0 or i == term or i == 1:                 # If in order to display a table
+            str_1 = str("%.2f" % (first_sum))                           # Strings for table output
             len_1 = 12 - len(str_1)
             str_2 = str(j)
             len_2 = 5 - len(str_2)
@@ -45,7 +45,7 @@ for i in range(1, term + 1):
             print("|", str_3, " " * len_3, "|", str_4, " " * len_4, "|", sep="", end="")
             print()
 
-        if j < 11:  # Interest infusion
+        if j < 11:                                                      # Interest infusion
             all_sum += infusion
 
         first_sum = all_sum
